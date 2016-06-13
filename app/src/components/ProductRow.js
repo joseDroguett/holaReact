dae085f6f3c1061369812019850d9373311a1f0e
José Droguett <jdroguett@produccion.datactil.com>
2016-06-12 22:12:16 -0400

    Primera parte, aprendiendo react

---
+++ app/src/components/ProductRow.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default class ProductRow extends React.Component {
+	constructor(){
+		super();
+	}
+	render(){
+		return(
+			<div>
+				<h6>{this.props.product}</h6>
+			</div>
+		);
+	}
+}
---
+++ app/src/components/ProductTable.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+import ProductCategoryRow from "./ProductCategoryRow.js";
+import ProductRow from "./ProductRow.js";
+
+export default class ProductTable extends React.Component {
+	constructor(){
+		super();
+	}
+	render(){
+		let rows = [];
+		let last_category = null;
+
+		if(this.props.products != null){
+			this.props.products.forEach((product) => {
+				if(product.category != last_category){
+					rows.push(<ProductCategoryRow category={product.category} key={product.category}/>);
+					last_category = product.category;
+				}
+				rows.push(<ProductRow product={product.name} key={product.name} />)
+			});
+		}else{
+			rows.push(<h1>Loading</h1>);
+		}
+
+		return(
+			<div>
+				{rows}
+			</div>
+		);
+	}
+}
